d197351d25292dade71be0a6b559cdea5c92b4af
burgertechtv <burgertechtv@gmail.com>
2024-12-25 13:19:06 -0500

    TempCalc Updates
    Made updates to TempCalc

--- tempcalc.js
+++ tempcalc.js
@@ -1,41 +1,47 @@
-//Daily Value type Const
-var kelvin = 290;
-//Convert Kelvin to Celsius
-var celsius = kelvin - 273;
-//Convert Celsius to Fahrenheit
-var fahrenheit = celsius*(9/5)+32;
-//Round Down
-fahrenheit = Math.floor(fahrenheit);
-
-console.log(kelvin);
-// document.getElementById("kelvinValue").innerHTML = "JS TEST";
-
-console.log(celsius);
-console.log(fahrenheit);
-console.log(
-  'The temperature is '
-  + fahrenheit +
-  ' degrees Fahrenheit.'
-);
-
-
 $(document).ready(function(){
-  $("#calc").one("click", function(){
-    $("#kelvinValue").append(kelvin);
-    $("#celsiusValue").append(celsius);
-    $("#fahrenheitValue").append(fahrenheit);
-  });
-  $("#calc2").one("click", function(){
-    $("#kelvinValue").append(kelvin);
-    $("#celsiusValue").append(celsius);
-    $("#fahrenheitValue").append(fahrenheit);
-  });
+  // $("#calc").one("click", function(){
+  //   $("#kelvinValue").append(kelvin);
+  //   $("#celsiusValue").append(celsius);
+  //   $("#fahrenheitValue").append(fahrenheit);
+  //     var tempType = $("#mySelect").val();
+  //     var inputNum = $("#inputNum").val();
+  //     console.log(tempType);
+  //     console.log(inputNum);
+  // });
   
-	$("#calc2").click(function() {
-	    var tc = $(this).closest("form").find("input[name='searchbox']").val();
-	    alert(tc);      
-	    return false;
-	});
+  $("form").submit(function(event) {
+      var tempType = $("#mySelect").val();
+      var inputNum = $("#inputNum").val();
+      var kelvin = inputNum;
+      var celsius = kelvin - 273;
+      //Convert Celsius to Fahrenheit
+      var fahrenheit = celsius*(9/5)+32;
+      //Round Down
+      fahrenheit = Math.floor(fahrenheit);
+
+      $("#kelvinValue").append(kelvin);       
+      $("#celsiusValue").append(celsius);
+      $("#fahrenheitValue").append(fahrenheit);
+
+      console.log(tempType);
+      console.log(inputNum);
+      console.log(kelvin);
+      console.log(celsius);
+      console.log(fahrenheit);
+
+      if (tempType === "k") {
+        alert("Temp Type = k");
+      } else if (tempType === "c") {
+        alert("Temp Type = c");
+      } else if (tempType === "f"){
+        alert("Temp Type = f");
+      } else {
+        alert("Default Value");
+      }
+
+      $("input.btn").prop('disabled', true);
+      event.preventDefault(); 
+  });
 
 });
 
